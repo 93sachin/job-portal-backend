9646e2821d44a27a25a204c880d9f4dd3d25c318
sachin <sachinesky@gmail.com>
2026-02-22 00:21:06 +0530

    Fix apply serializer

--- applications/serializers.py
+++ applications/serializers.py
@@ -1,14 +1,7 @@
 from rest_framework import serializers
 from .models import Application
 
-
 class ApplicationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Application
-        fields = ["id", "job", "applied_at"]
-        read_only_fields = ["id", "applied_at"]
-
-    def create(self, validated_data):
-        request = self.context.get("request")
-        validated_data["applicant"] = request.user
-        return super().create(validated_data)
+        fields = ['id', 'job']
