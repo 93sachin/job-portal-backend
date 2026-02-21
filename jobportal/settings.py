74bc8fa85af7280ae1571d00ba0dce9378f739d7
sachin <sachinesky@gmail.com>
2026-02-22 00:00:33 +0530

    Increase JWT lifetime

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -151,3 +151,10 @@ CORS_ALLOW_HEADERS = [
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
 ]
+
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+}
