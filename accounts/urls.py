dc487548bd513f0dffaa9f89a49be624905bfe1f
sachin <sachinesky@gmail.com>
2026-02-22 22:56:30 +0530

    Add current user API endpoint for role-based auth

---
+++ accounts/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from .views import CurrentUserView
+urlpatterns = [
+    path("user/", CurrentUserView.as_view()),
+]
--- jobportal/urls.py
+++ jobportal/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
 
     path('api/', include('api.urls')),
     path('api/applications/', include('applications.urls')),
+    path('api/', include('accounts.urls')),
 ]
