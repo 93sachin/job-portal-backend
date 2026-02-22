34de68d8bcc5ed1f5083fd52051abab672d1fbbc
sachin <sachinesky@gmail.com>
2026-02-23 00:17:26 +0530

    Added profile API

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from .views import CurrentUserView
+from .views import profile_view
+
 urlpatterns = [
     path("user/", CurrentUserView.as_view()),
+    path('profile/', profile_view),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -10,3 +10,17 @@ class CurrentUserView(APIView):
             "username": request.user.username,
             "role": request.user.role
         })
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def profile_view(request):
+    user = request.user
+    return Response({
+        "username": user.username,
+        "email": user.email,
+        "id": user.id
+    })
